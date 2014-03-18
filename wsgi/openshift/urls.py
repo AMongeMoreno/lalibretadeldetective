399816308e79b9323cdccc60cb08888e1782f700
Shagrash <andres.monmor@gmail.com>
2014-03-18 02:30:49 +0100

    ddd

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^$', 'views.home', name='home'),
+    url(r'^home/', 'views.home', name='home'),
 )
