83a40516d2a3e5e384b3e96a916621092155e12c
unknown <amonge@.ayesa.es>
2014-03-18 09:27:35 +0100

    gitignore

--- wsgi/openshift/LaLibretaDelDetective/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-import LaLibretaDelDetective 
-
-urlpatterns = patterns('',
-    url(r'^$', LaLibretaDelDetective.views.index, name='index')
-)
--- wsgi/openshift/LaLibretaDelDetective/views.py
+++
@@ -1,5 +0,0 @@
-# Create your views here.
-from django.shortcuts import render
-def index(request):
-    context = {}
-    return render(request, 'LaLibretaDelDetective/index.html', context)
